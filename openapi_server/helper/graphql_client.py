e082d0545b820320af4d7dd532bac7161ca09e9b
william.le-roux <william.leroux@indykite.com>
2023-01-24 17:09:04 +0100

    Adding KnowledgeGraphqlClient

---
+++ openapi_server/helper/graphql_client.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import abort
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
+
+class KnowledgeGraphqlClient:
+    def __init__(self):
+        # Select your transport with a defined url endpoint
+        self.addr = os.getenv('INDYKITE_KNOWLEDGE_GRAPH_ADDRESS')
+        if not self.addr:
+            abort(503, "Missing INDYKITE_KNOWLEDGE_GRAPH_ADDRESS environment variable")
+        self.transport = None
+        self.client = None
+
+    def set_authorization(self, token):
+        self.transport = RequestsHTTPTransport(
+            url=self.addr,
+            verify=True,
+            retries=3,
+            headers={"Authorization": token},
+        )
+        # Create a GraphQL client using the defined transport
+        self.client = Client(transport=self.transport, fetch_schema_from_transport=True)
+
+    def execute(self, query):
+        if not self.client:
+            abort(503, "KnowledgeGraphqlClient client is not initialized")
+        return self.client.execute(query)
+
+    # Provide a GraphQL query
+    query = gql(
+        """
+        query getContinents {
+          continents {
+            code
+            name
+          }
+        }
+    """
+    )
