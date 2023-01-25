e7449dfb505b1d95c2bfe8801f604ca8b72a58f7
Natalia <natalia_bakos@yahoo.com>
2023-01-25 14:16:09 +0100

    Fix create invitation and add get invitation

--- openapi_server/controllers/invitation_controller_.py
+++ openapi_server/controllers/invitation_controller_.py
@@ -1,11 +1,11 @@
 import openapi_server.controllers.security_controller_ as sec
+import openapi_server.helper.format_helper as helper
 
-from google.protobuf import struct_pb2
 from indykite_sdk.indykite.identity.v1beta2 import identity_management_api_pb2 as pb2
 from indykite_sdk.indykite.objects.v1beta1 import struct_pb2 as objects
 
 
-def create_invitation(token, invitee_email):
+def create_invitation(token, invitee_email, reference_id):
   client = sec.create_identity_client_connection()
   try:
     intospect_response = sec.introspect_token(client, token)
@@ -15,19 +15,39 @@ def create_invitation(token, invitee_email):
   try:
     response = client.stub.CreateInvitation(
       pb2.CreateInvitationRequest(
-        tenant_id=intospect_response['digitalTwin']['digitalTwin']['tenantId'],
+        tenant_id=intospect_response['tokenInfo']['subject']['tenantId'],
         message_attributes=objects.MapValue(
           fields={
-            "inviter_id": objects.Value(string_value=intospect_response['digitalTwin']['digitalTwin']['id'])
+            "inviter_id": objects.Value(string_value=intospect_response['tokenInfo']['subject']['id'])
           }
         ),
-        email=invitee_email
+        email=invitee_email,
+        reference_id=reference_id
       )
     )
     print("Response of the create invitation:\n%s" % response)
   except Exception as e:
-    print(e)
+    print("Exception: %s" % e)
     return e
 
   sec.close_identity_client_connection(client)
   return None
+
+
+def get_one_invitation(token, invitation_id):
+  client = sec.create_identity_client_connection()
+  print(invitation_id)
+
+  try:
+    response = client.stub.CheckInvitationState(
+      pb2.CheckInvitationStateRequest(
+        reference_id=invitation_id
+      )
+    )
+    print("Response of the get invitation:\n%s" % response)
+  except Exception as e:
+    print(e)
+    return None
+
+  sec.close_identity_client_connection(client)
+  return helper.decode_response(response)
