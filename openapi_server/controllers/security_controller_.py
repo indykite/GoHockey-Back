ecbdd6ef002450f05f3b9faee1eaa4bafbdab28f
Natalia <natalia_bakos@yahoo.com>
2023-01-25 11:29:09 +0100

    Add SDK connection create/close and introspect token

--- openapi_server/controllers/security_controller_.py
+++ openapi_server/controllers/security_controller_.py
@@ -1,4 +1,9 @@
+import logging
+import openapi_server.helper.format_helper as helper
+
 from typing import List
+from indykite_sdk.identity import IdentityClient
+from indykite_sdk.indykite.identity.v1beta2 import identity_management_api_pb2 as pb2
 
 
 def info_from_bearerAuth(token):
@@ -13,3 +18,35 @@ def info_from_bearerAuth(token):
     :rtype: dict | None
     """
     return {'indykite_token': token}
+
+
+def create_identity_client_connection():
+    try:
+        client = IdentityClient(False)
+        return client
+    except Exception as e:
+        logging.debug("Failed to open Client connection to Indykite: %s" % e)
+        return e
+
+
+def close_identity_client_connection(client):
+    try:
+        client.channel.close()
+        logging.info("Client connection has been closed")
+    except Exception as e:
+        logging.debug("Failed to close the Client connection to Indykite: %s" % e)
+
+
+def introspect_token(client, token):
+    try:
+        resp = client.stub.TokenIntrospect(
+            pb2.TokenIntrospectRequest(token=token)
+        )
+        formatted_response = helper.decode_response(resp)
+        logging.info("Token introspection was success for the following digital twin: %s"
+                     % formatted_response['tokenInfo']['customerId'])
+    except Exception as e:
+        logging.info("Introspection failed: %s" % e)
+        return None
+
+    return formatted_response
