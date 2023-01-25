9a8861ebc3bb676a7147a22565e911f9d8cecc83
Natalia <natalia_bakos@yahoo.com>
2023-01-25 11:28:40 +0100

    Add invitation to openapi

--- openapi_server/models/__init__.py
+++ openapi_server/models/__init__.py
@@ -5,6 +5,9 @@ from __future__ import absolute_import
 # import models into model package
 from openapi_server.models.inline_response200 import InlineResponse200
 from openapi_server.models.inline_response2001 import InlineResponse2001
+from openapi_server.models.invitation_create_body import InvitationCreateBody
+from openapi_server.models.invitation_information_body import InvitationInformationBody
+from openapi_server.models.invitation_information_body_message_attributes_inner import InvitationInformationBodyMessageAttributesInner
 from openapi_server.models.user_address_body import UserAddressBody
 from openapi_server.models.user_child_body import UserChildBody
 from openapi_server.models.user_subscription_body import UserSubscriptionBody
