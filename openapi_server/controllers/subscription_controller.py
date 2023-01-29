c4a122ada437d3036ec491c88bfd1d696631414e
Natalia <natalia_bakos@yahoo.com>
2023-01-29 18:13:02 +0100

    Add basic subscription calls

--- openapi_server/controllers/subscription_controller.py
+++ openapi_server/controllers/subscription_controller.py
@@ -2,36 +2,27 @@ import connexion
 
 from openapi_server.models.inline_response2001 import InlineResponse2001  # noqa: E501
 from openapi_server.models.user_subscription_body import UserSubscriptionBody  # noqa: E501
+from openapi_server.graphql_queries.get_subscription import get_subscription_query
+from flask import abort, g
 
 
-def user_subscriptions_get():  # noqa: E501
+def user_subscriptions_get(child_id):  # noqa: E501
     """Get the subscriptions of the logged in user
 
      # noqa: E501
     """
-    return [
-        {
-            "from": "2022-01-01T00:00:00Z",
-            "to": "2022-12-31T23:59:59Z",
-            "sku": [
-                "SKU22005",
-                "SKU22005"
-            ],
-            "child": "123e4567-e89b-12d3-a456-426655440000"
-        },
-        {
-            "from": "2022-02-02T00:00:00Z",
-            "to": "2022-11-30T23:59:59Z",
-            "sku": [
-                "SKU22006",
-                "SKU22006"
-            ],
-            "child": "123e4567-e89b-12d3-a456-426655440000"
+    get_subscription_param = {
+          "where": {
+            "child": {
+              "externalId": child_id
+            }
+          }
         }
-    ]
+    subscription = g.indykite_graph_client.execute(get_subscription_query, get_subscription_param)
+    return subscription
 
 
-def user_subscription_get():  # noqa: E501
+def user_subscription_get(subscription_id):  # noqa: E501
     """Get the subscription of the logged in user
 
      # noqa: E501
@@ -39,15 +30,13 @@ def user_subscription_get():  # noqa: E501
 
     :rtype: InlineResponse2001
     """
-    return {
-        "from": "2022-01-01T00:00:00Z",
-        "to": "2022-12-31T23:59:59Z",
-        "sku": [
-            "SKU22005",
-            "SKU22005"
-        ],
-        "child": "123e4567-e89b-12d3-a456-426655440000"
-    }
+    get_subscription_param = {
+          "where": {
+            "externalId": subscription_id
+          }
+        }
+    subscription = g.indykite_graph_client.execute(get_subscription_query, get_subscription_param)
+    return subscription
 
 
 def user_subscription_post(user_subscription_body=None):  # noqa: E501
@@ -62,11 +51,5 @@ def user_subscription_post(user_subscription_body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         user_subscription_body = UserSubscriptionBody.from_dict(connexion.request.get_json())  # noqa: E501
-    return {
-        "from": "2022-01-01T00:00:00Z",
-        "to": "2022-12-31T23:59:59Z",
-        "sku": [
-            "SKU22005"
-        ],
-        "child": "123e4567-e89b-12d3-a456-426655440000"
-    }
+    subscription = g.indykite_graph_client.execute(get_subscription_query, user_subscription_body)
+    return subscription
