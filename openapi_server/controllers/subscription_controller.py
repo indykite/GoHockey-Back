a631af6f217958a07d9da0fa3cdb7d6865cd8328
william.le-roux <william.leroux@indykite.com>
2023-01-31 12:18:53 +0100

    Implement subscription patch

--- openapi_server/controllers/subscription_controller.py
+++ openapi_server/controllers/subscription_controller.py
@@ -1,5 +1,7 @@
 import connexion
+from flask import g, abort
 
+from openapi_server.graphql_queries.patch_subscription import patch_subscription_mutation
 from openapi_server.models.inline_response2001 import InlineResponse2001  # noqa: E501
 from openapi_server.models.user_subscription_body import UserSubscriptionBody  # noqa: E501
 
@@ -70,3 +72,31 @@ def user_subscription_post(user_subscription_body=None):  # noqa: E501
         ],
         "child": "123e4567-e89b-12d3-a456-426655440000"
     }
+
+
+def user_subscription_patch(token_info, subscription_id):  # noqa: E501
+    """Update the subscription of the logged in user
+
+     # noqa: E501
+
+    :param token_info:
+    :param subscription_id: Id of the subscription to update
+    :type subscription_id: str
+
+    :rtype: None
+    """
+    digital_twin = g.indykite_client.get_digital_twin_by_token(token_info['indykite_token'], [])
+    if digital_twin is None:
+        return abort(404, description="Resource not found")
+    if connexion.request.is_json:
+        data = UserSubscriptionBody.from_dict(connexion.request.get_json())
+    patch_subscription_params = {
+        "where": {
+            "externalId": subscription_id
+        },
+        "update": {
+            "valid_to": data.to
+        }
+    }
+    child = g.indykite_graph_client.execute(patch_subscription_mutation, patch_subscription_params)
+    return child
